d625ea2ffc630471da1f93eb672520bc0bc8b06e
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-29 08:16:15 +0300

    remove unused

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
